451d4e56d1f3fc19692408ff5c51b84126dfc78f
Aleksei Ivanov <profpopoff@gmail.com>
2023-02-22 12:31:31 +0300

    Add Posts component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
+import Posts from '@/components/Posts/Posts'
 import styles from './page.module.css'
 
-export default function Home() {
+async function getData() {
+  const res = await fetch('https://news.itmo.ru/api/news/list/?ver=2.0&per_page=9', { cache: "no-store" })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+
+  const data = await getData()
+
   return (
     <main className={styles.main}>
-      <h1>posts</h1>
+      <h1 className={styles.title}>Новости и&nbsp;события</h1>
+      <Posts events={data.news} />
     </main>
   )
 }
---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,12 @@
+import { Post as PostInterface } from "@/interfaces/Post"
+import Post from "./Post/Post"
+
+import styles from './Posts.module.css'
+
+export default function Posts({ events }: { events: PostInterface[] }) {
+   return (
+      <div className={styles.posts}>
+         {events.map(event => <Post title={event.title} image={event.image_big} date={event.date} />)}
+      </div>
+   )
+}
