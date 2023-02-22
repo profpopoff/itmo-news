65be5c89e660312354d6c985a7862689f21e1be3
Aleksei Ivanov <profpopoff@gmail.com>
2023-02-22 21:37:01 +0300

    Add Loading indicator

---
+++ src/components/Posts/Loading/Loading.tsx
@@ -0,0 +1,24 @@
+import postsStyles from '../Posts.module.css'
+import postStyles from '../Post/Post.module.css'
+import styles from './Loading.module.css'
+
+export default function Loading() {
+   return (
+      <div className={postsStyles.posts}>
+         {[...Array(9)].map((i) => (
+            <div className={`${postStyles.post} ${styles.post}`}>
+               <div className={styles.wrapper}>
+
+                  <div className={postStyles.image}>
+                     <span className={styles.greyArea}></span>
+                  </div>
+                  <div className={postStyles.headline}>
+                     <p className={postStyles.date}>12 Lorem 2022</p>
+                     <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit voluptatem magni quod error repellendus ut quisquam recusandae.</h2>
+                  </div>
+               </div>
+            </div>
+         ))}
+      </div>
+   )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,46 @@
 import Layout from '@/components/Layout'
+import Loading from '@/components/Posts/Loading/Loading'
 import Posts from '@/components/Posts/Posts'
 import { selectData, setData } from '@/redux/slices/data'
 import { selectLanguage } from '@/redux/slices/language'
 import styles from '@/styles/pages/HomePage.module.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 export default function Home() {
 
    const dispatch = useDispatch()
-
    const eventsdata = useSelector(selectData)
-
    const currentLanguage = useSelector(selectLanguage)
 
+   const [isLoading, setIsLoading] = useState(false)
+
    useEffect(() => {
-      fetchData(currentLanguage === 'Eng' ? 2 : 1)
+      const getData = async () => {
+         let isDone = false
+
+         fetchData(currentLanguage === 'Eng' ? 2 : 1)
+            .then(() => isDone = true)
+
+         await delay(250)
+
+         const checkIsDone = async () => {
+            if (!isDone) {
+               setIsLoading(true)
+               await delay(500)
+               checkIsDone()
+            } else {
+               setIsLoading(false)
+            }
+         }
+         checkIsDone()
+      }
+      getData()
    }, [currentLanguage])
+
+   const delay = (ms: number) => {
+      return new Promise(resolve => setTimeout(resolve, ms))
+   }
 
    const fetchData = async (language: number) => {
       const res = await fetch(`https://news.itmo.ru/api/news/list/?ver=2.0&language_id=${language}&per_page=9`)
@@ -27,7 +51,7 @@ export default function Home() {
    return (
       <Layout>
          <h1 className={styles.title}>{currentLanguage === 'Рус' ? 'Новости и события' : 'News and Events'}</h1>
-         {!!eventsdata ? <Posts events={eventsdata.news} /> : <>loading</>}
+         {isLoading ? <Loading /> : !!eventsdata && <Posts events={eventsdata.news} />}
       </Layout>
    )
 }
