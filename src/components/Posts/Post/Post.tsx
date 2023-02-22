e0730e6165b321d82026cf3cbba24980ad1e0950
Aleksei Ivanov <profpopoff@gmail.com>
2023-02-22 12:53:47 +0300

    Make Post a Link to specific event page

--- src/components/Posts/Post/Post.tsx
+++ src/components/Posts/Post/Post.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './Post.module.css'
 
-export default function Post({ title, image, date }: { title: string, image: string, date: Date }) {
+export default function Post({ id, title, image, date }: { id: number, title: string, image: string, date: Date }) {
    return (
-      <div className={styles.post}>
+      <Link href={`/event/${id}`} className={styles.post}>
          <div className={styles.image}>
             <Image
                className={styles.src}
@@ -14,9 +15,9 @@ export default function Post({ title, image, date }: { title: string, image: str
             />
          </div>
          <div className={styles.headline}>
-            <p className={styles.date}>{new Date(date).toLocaleDateString('ru', { year:"numeric", month:"long", day:"numeric"})}</p>
+            <p className={styles.date}>{new Date(date).toLocaleDateString('ru', { year: "numeric", month: "long", day: "numeric" })}</p>
             <h2 title={title}>{title}</h2>
          </div>
-      </div>
+      </Link>
    )
 }
